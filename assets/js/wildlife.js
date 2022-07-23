617b62fcccbf8f27e47e132f570c9ea150a7614b
Francisca-Heii <franciscahheii@gmail.com>
2022-07-23 12:43:52 +0100

    Created wildlife folder,  added relevant images, and created wildlife.js to add show more script

---
+++ assets/js/wildlife.js
@@ -0,0 +1,10 @@
+// Okavango wildlife show more button
+function showMoreAnimals() {
+    var x = document.getElementById("show-hide-animals");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
